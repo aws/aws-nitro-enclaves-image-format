4e022aa7f20cbd31ed6da0d3fd6b8e32d3e61b39
Sabin Rapan <sabrapan@amazon.com>
2022-04-23 11:11:30 +0000

    Import from aws-nitro-enclaves-cli
    ... from source code at
    https://github.com/aws/aws-nitro-enclaves-cli/releases/tag/v1.2.0
    
    Signed-off-by: Sabin Rapan <sabrapan@amazon.com>

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+// Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+//! AWS Nitro Enclaves Image Format API
+//!
+//! This is the library that provides the API for interacting with Enclave Image Format (EIF) files
+//! used in AWS Nitro Enclaves.
+//!
+//! defs provides the APIs that define an EIF and related objects.
+//! utils provides the APIs to consume and interact with an EIF and related objects.
+
+pub mod defs;
+pub mod utils;
